40836881a95d525bccf80a0860a7813268cf5a6b
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-27 22:07:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
+  delete Object.key;
   return obj;
 }
