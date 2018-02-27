956fd2c00e9488aa2a3e9cb3f27d67330c4ac250
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-27 21:55:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = {};
+  
+var obj = { prop: 1 };
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({},obj,{[key]:value});
+}
+
+var obj = { prop: 1 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
