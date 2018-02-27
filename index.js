b369b77dc0b7ee3e19288c7b9aaf6c27c618e227
Abdoulrahime Diallo <diallo.abdoulrahime@gmail.com>
2018-02-27 22:12:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 var obj = { prop: 1 }
+var newObj = Object.assign({}, object);
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
   delete newObj.key;
   return newObj;
 }
